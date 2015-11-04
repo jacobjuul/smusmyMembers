5c9c62e47a75a99518fe550cd4d35cd7254896db
Jacob Juul Christiansen <jacob.juul@gmail.com>
2015-11-04 21:49:32 +0100

    more

--- models/house.js
+++ models/house.js
@@ -2,24 +2,31 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var HouseSchema = new Schema({
-    headline: String,
-    description: String,
-    location: {
-        city: String,
-        street: String,
-        number: Number,
-        zipCode: Number,
-    },
-    images: [{
-        url: String
+  headline: String,
+  description: String,
+  location: {
+    city: String,
+    street: String,
+    number: Number,
+    zipCode: Number,
+  },
+  images: [{
+    url: String
     }],
-    size: Number,
-    floor: Number,
-    year: Number,
-    created_at: Date,
-    updated_at: Date
+  size: Number,
+  floor: Number,
+  year: Number,
+  created_at: Date,
+  updated_at: Date
+});
+
+HouseSchema.pre('save', function(next) {
+  if (!this.created_at) {
+    this.created_at = new Date();
+  }
 });
 
 var House = mongoose.model('House', HouseSchema);
 
 module.exports = House;
+
--- routes/index.js
+++ routes/index.js
@@ -6,31 +6,50 @@ var bodyParser = require('body-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', {
-        title: 'Express'
-    });
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 router.get('/cribs', function(req, res) {
-    House.find({}, function(err, houses) {
-        if (err)
-            throw err;
+  House.find({}, function(err, houses) {
+    if (err)
+      throw err;
 
-        res.render('index', {
-            houses: houses
-        });
+    res.render('index', {
+      houses: houses
     });
+  });
 });
 
-router.post('/crib', function(req, res) {
+
+router.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
+});
+
+router.route('/crib/:cribId')
+  .post(function(req, res) {
     var crib = new House(req.body);
-
     crib.save(function(err) {
-        if (err)
-            throw err;
+      if (err)
+        throw err;
 
-        res.redirect('/cribs');
+      res.redirect('/cribs');
     });
-});
+  })
+  .get(function(req, res) {
+    House.findById(req.params.cribId, function(err, crib) {
+      if (err)
+        throw err;
+
+      res.render('crib', {
+        crib: crib
+      });
+    });
+  });
+
+
 
 module.exports = router;
+
